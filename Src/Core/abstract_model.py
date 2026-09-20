18c86c6ce1c6bd69d1c47ea9eab23642d8d6101b
sensei38 <shihowdima1337@gmail.com>
2026-09-20 22:08:46 +0800

    changed proj structure

--- Src/Core/abstract_model.py
+++ Src/Core/abstract_model.py
@@ -2,18 +2,20 @@ from abc import ABC
 import uuid
 
 class name_id(ABC):
+    # Конструктор абстрактного класса
     def __init__(self):
         self.__name = ""
         self.__id = uuid.uuid4()
 
+    # Возвращает id 
     @property
     def id(self):
         return self.__id
-
+    # Возвращает имя
     @property
     def name(self) -> str:
         return self.__name
-
+    # Задает новое имя
     @name.setter
     def name(self, new_name: str):
         if new_name is not None and len(new_name) > 0:
